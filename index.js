fdd6fe77fb3949a4ea3ff343179571293f41c4b5
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-06-10 00:03:42 +0300

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
+     const key = [
+  "What Does The This Keyword Mean?",
+  "What Is The Constructor OO Pattern?",
+  "Implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What Is NaN And How Can We Check For It",
+  "What Is The Difference Between StopPropagation And PreventDefault?",
+  "Immutable State And Pure Functions",
+  "What Is The Difference Between == And ===?",
+  "What Is The Difference Between Event Capturing And Bubbling?",
+  "What Is JSONP?"
+]
 const titleCased = () => {
-  return tutorials
+
+  return key.map((array) => {
+      const ar_array = array.split(' ');
+      const ar_new = ar_array.map((string) => {
+          string = `${string[0].toUpperCase()}${string.slice(1)}`;
+          return string;
+      })
+      return ar_new.join(" ");
+  })
+  return key
 }
+
+console.log(titleCased())
